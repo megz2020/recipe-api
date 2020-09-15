8ad28dedd26176bc41e769b64b6040e26585da48
a.eissa <a.eissa@evorezm.com>
2020-09-15 16:29:42 +0200

    updated admin to support super user

--- app/core/models.py
+++ app/core/models.py
@@ -20,11 +20,8 @@ class UserManager(BaseUserManager):
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
-        user.save(using=self._db)
-        
+        user.save(using=self._db)  
         return user
-        
-
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -32,7 +29,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    is_stuff = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects=UserManager()
 
